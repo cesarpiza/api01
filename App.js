7d39de252691b0dd9cae960b2dc8cb5f1fe34999
CesarPiza <138241111+CesarPiza@users.noreply.github.com>
2023-11-17 20:18:37 -0300

    mudei a lógica usando filter
    ...

--- App.js
+++ App.js
@@ -25,14 +25,35 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [data, setData] = useState([]);
   const [hasMoreData, setHasMoreData] = useState(true);
+  
+  // Versão com axios
+  // const fetchApi = async () => {
+  //   if (!hasMoreData) return;
+  //   const { data } = await axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`);
+
+  //   if (data.results) {
+  //     const current = data.results;
+  //     setData(prev => [...prev, ...current]);
+
+  //     if (data.info?.next) {
+  //       setPage(prev => prev + 1);
+  //     } else {
+  //       setHasMoreData(false);
+  //     }
+
+  //   }
+
+  // }
 
   const fetchApi = async () => {
     if (!hasMoreData) return;
-    const { data } = await axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`);
+    const response = await fetch(`https://rickandmortyapi.com/api/episode?page=${page}`);
+    const data = await response.json();
 
     if (data.results) {
       const current = data.results;
-      setData(prev => [...prev, ...current]);
+      // Tive que usar esse lógica: current.filter... para garantir que não haja cópias de itens/objetos no setData/data. Principalmente com o uso do fetch, no qual estava tendo problemas ao renderizar os novos itens/objetos. Com o uso do axios é mais tranquilo. De qualquer forma essa lógica garante que não haja dublicatas.
+      setData(prev => [...prev, ...current.filter(item => !prev.some(existingItem => existingItem.id === item.id))]);
 
       if (data.info?.next) {
         setPage(prev => prev + 1);
